2ba6416446c37e28061526e9dc57c69d9169b544
Boris Ushakov <inlvert@gmail.com>
2025-09-16 14:45:30 +0300

    add db connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { url } from 'inspector';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_CONNECTION'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
