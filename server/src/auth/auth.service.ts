a569aa9b9e1715a3eb3ce9a6992645fb90d37d7e
Boris Ushakov <inlvert@gmail.com>
2025-09-25 13:49:29 +0300

    add 4

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
 
@@ -14,7 +14,7 @@ export class AuthService {
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
 
-    const matchPassword = await bcrypt.compare(password, user?.password);
+    const matchPassword = await bcrypt.compare(password, user!.password);
 
     console.log('matchPassword', matchPassword);
 
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import bcrypt from 'node_modules/bcryptjs';
+import bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
